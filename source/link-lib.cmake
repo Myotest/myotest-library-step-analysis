9ed0b92a63ed979ee5a11617ae6ab9c278fcbd7b
std <std@myotest.com>
2016-02-17 13:58:04 +0100

    - change library name

--- source/link-lib.cmake
+++ source/link-lib.cmake
@@ -2,5 +2,5 @@
 # use the target_link_libraries command to instruct CMake to add our custom
 # library as a dependency of this module's library (which always has the same
 # name as the module itself)
-target_link_libraries(myotest-step-analysis-library "${CMAKE_CURRENT_LIST_DIR}/myotest-step-analysis-library.a")
+target_link_libraries(myotest-library-step-analysis "${CMAKE_CURRENT_LIST_DIR}/myotest-library-step-analysis.a")
 
